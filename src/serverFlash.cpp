395738692b7bee16775ff3ae7772bc3db44b4119
Texnopark <your_email@example.com>
2025-09-24 12:49:27 +0400

    chnages

---
+++ src/serverFlash.cpp
@@ -0,0 +1,50 @@
+#include "serverFlash.h"
+
+// Global değişken tanımı
+HTTPClient http;
+
+bool otaUpdate(String filename)
+{
+    String url = "https://sparkbot.correcttechno.com/compiler/arduino/codes/" + filename + "/build/esp32.esp32.esp32/" + filename + ".ino.bin";
+    Serial.println(url);
+
+    http.begin(url);
+    int httpCode = http.GET();
+
+    if (httpCode == HTTP_CODE_OK)
+    {
+        int contentLength = http.getSize();
+        WiFiClient *stream = http.getStreamPtr();
+
+        if (!Update.begin(contentLength)) {
+            return false;
+        }
+
+        size_t written = Update.writeStream(*stream);
+        if (written != contentLength) {
+            Serial.printf("Eksik yazıldı: %d / %d\n", written, contentLength);
+            return false;
+        }
+
+        if (Update.end()) {
+            if (Update.isFinished()) {
+                Serial.println("Başarılı! Yeniden başlatılıyor...");
+                ESP.restart();
+                return true;
+            } else {
+                Serial.println("Update bitmedi!");
+                return false;
+            }
+        } else {
+            Serial.printf("Update hatası: %s\n", Update.errorString());
+            return false;
+        }
+    }
+    else
+    {
+        Serial.printf("HTTP hata kodu: %d\n", httpCode);
+        return false;
+    }
+
+    http.end();
+}
--- src/serverFlash.h
+++ src/serverFlash.h
@@ -1,63 +1,14 @@
+#ifndef SERVER_FLASH_H
+#define SERVER_FLASH_H
+
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <Update.h>
 
+// Global değişken bildirimi
+extern HTTPClient http;
 
-HTTPClient http;
+// Fonksiyon prototipi
+bool otaUpdate(String filename);
 
-bool otaUpdate(String filename)
-{
-    String url=String("https://sparkbot.correcttechno.com/compiler/arduino/codes/"+filename+"/build/esp32.esp32.esp32/") + filename+".ino.bin";
-    Serial.println(url);
-    http.begin(url);
-    int httpCode = http.GET();
-
-    if (httpCode == HTTP_CODE_OK)
-    {
-        int contentLength = http.getSize();
-        WiFiClient *stream = http.getStreamPtr();
-
-        if (!Update.begin(contentLength))
-        {
-            return false;
-        }
-
-        size_t written = Update.writeStream(*stream);
-        if (written == contentLength)
-        {
-            Serial.println("Update tamamlandı!");
-            return false;
-        }
-        else
-        {
-            Serial.printf("Eksik yazıldı: %d / %d\n", written, contentLength);
-            return false;
-        }
-
-        if (Update.end())
-        {
-            if (Update.isFinished())
-            {
-                Serial.println("Başarılı! Yeniden başlatılıyor...");
-                return true;
-                ESP.restart();
-            }
-            else
-            {
-                Serial.println("Update bitmedi!");
-                return false;
-            }
-        }
-        else
-        {
-            Serial.printf("Update hatası: %s\n", Update.errorString());
-            return false;
-        }
-    }
-    else
-    {
-        Serial.printf("HTTP hata kodu: %d\n", httpCode);
-        return false;
-    }
-    http.end();
-}
+#endif
